5e7547c70f4d1e2430b430be02d3accb47b5b612
jiro.octavo <jiro.octavo@gmail.com>
2023-07-10 16:03:58 +0800

    chore: uses boilerplate for the todo app

---
+++ src/Another.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Another = (props) => {
+    return (
+        <div>
+            Another Component, { props.name }
+        </div>
+    );
+}
+
+export default Another;
